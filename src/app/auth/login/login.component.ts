03c74ea2e4b160bf338a80b6b75bd43d48c0e16d
aya atef <ayaatef21ayaatef@outlook.com>
2024-08-16 15:03:12 +0300

    file structure

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
---
+++ src/app/web-site/website-landing/website-landing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-website-landing',
+  standalone: true,
+  imports: [],
+  templateUrl: './website-landing.component.html',
+  styleUrl: './website-landing.component.scss'
+})
+export class WebsiteLandingComponent {
+
+}
